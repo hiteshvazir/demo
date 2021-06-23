f2d9d728a677df16ef9454584860afe8490952dc
git user name <hiteshinfotech007@gmail.com>
2021-06-23 10:51:12 +0530

    first commit

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return "<h1>This is my First flask application </h1>"
+    return "<h1>This Is My First flask application </h1>"
 
 
 if __name__ == "__main__":
